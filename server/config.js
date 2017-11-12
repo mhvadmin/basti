db0e5869a407151fa9ce58bffe5edcb702d5e1d8
mhvadmin <mhvadmin@myhomevighanga.com>
2017-11-12 18:42:52 +0530

    server component added

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    AUTH0_DOMAIN: 'myhomevihanga.auth0.com', // e.g., kmaida.auth0.com
+    AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://mhv:mhv2017@ds133004.mlab.com:33004/mhv'
+  };
